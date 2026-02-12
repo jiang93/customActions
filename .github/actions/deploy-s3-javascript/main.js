f1f309a0c313689d361a77ae3ab829af73df26c1
jiang93 <jianghy1993@hotmail.com>
2026-02-12 17:06:18 +0800

    Deploy AWS S3 Bucket

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,16 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Hello from my custom JavaScript Action!')
+    // 1) get inputs
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket_region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2) upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    core.notice('Hello from my custom JavaScript Action!');
 }
 
 run();
